8a29f9a74d898dd03f8f6814a95cdd7bf51ed28d
arafat1023 <arafathossain847@gmail.com>
2024-12-31 16:34:05 +0600

    add database

--- pagination-example/pagination.php
+++ pagination-example/pagination.php
@@ -1,8 +1,5 @@
 <?php
-$host = "localhost";
-$dbname = "laravel_database";
-$username = "root";
-$password = "Iamgenious12@";
+require 'db.php';
 
 try{
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
@@ -13,25 +10,5 @@ catch (PDOException $e){
     echo "Connection failed: " . $e->getMessage();
     die("Connection failed: " . $e->getMessage());
 }
-
-$recordsPerPage = 2;
-$stmt= $pdo->query("SELECT Count(*) As total_records from projects");
-$totalRecords = $stmt->fetch()['total_records'];
-$totalPages = ceil($totalRecords / $recordsPerPage);
-echo "Total Records: " . $totalRecords;
-echo "total pages: ". $totalPages;
-
-$currentPage = isset($_GET['page']) ? (int)$_GET['page'] : 1;
-$currentPage = max(1, min($currentPage, $totalPages)); // Ensure valid page
-$offset = ($currentPage - 1) * $recordsPerPage;
-echo "<p>Current Page: $currentPage</p>";
-echo "<p>Offset: $offset</p>";
-
-$stmt = $pdo->prepare("SELECT * FROM projects LIMIT :offset, :limit");
-$stmt->bindValue(':limit', $recordsPerPage, PDO::PARAM_INT);
-$stmt->bindValue(':offset', $offset, PDO::PARAM_INT);
-$stmt->execute();
-$projects = $stmt->fetchAll(PDO::FETCH_ASSOC);
-echo "<p>Total Records: " . count($projects). " records for the pages</p>";
 ?>
 
