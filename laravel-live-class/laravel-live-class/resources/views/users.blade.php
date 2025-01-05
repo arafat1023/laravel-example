4994fdc565a79c6cf520533bd513e470f8c60e24
arafat1023 <arafathossain847@gmail.com>
2025-01-05 17:08:35 +0600

    feat: show layout data

--- laravel-live-class/laravel-live-class/resources/views/users.blade.php
+++ laravel-live-class/laravel-live-class/resources/views/users.blade.php
@@ -1,9 +1,15 @@
-<ul>
-    @if (count($users) > 2)
-        @foreach ($users as $user)
-            <li>{{ $user->name }}</li>
-        @endforeach
-    @else
-        <p>No users found.</p>
-    @endif
-</ul>
+@extends('layouts.app')
+
+@section('title', 'User List')
+
+@section('content')
+    <ul>
+        @if (count($users) > 0)
+            @foreach ($users as $user)
+                <li>{{ $user->name }}</li>
+            @endforeach
+        @else
+            <p>No users found.</p>
+        @endif
+    </ul>
+@endsection
--- laravel-live-class/laravel-live-class/routes/web.php
+++ laravel-live-class/laravel-live-class/routes/web.php
@@ -4,8 +4,8 @@ use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return 'hello world';
-//    return view('welcome');
+//    return 'hello world';
+    return view('home');
 });
 
 Route::get('/greeting', function () {
