34ac3a3c1a8dbdbe321541b85382e9692e730598
arafat1023 <arafathossain847@gmail.com>
2025-01-05 16:51:18 +0600

    feat: show conditional data in blade template

--- laravel-live-class/laravel-live-class/resources/views/users.blade.php
+++ laravel-live-class/laravel-live-class/resources/views/users.blade.php
@@ -1,4 +1,9 @@
-@foreach($users as $user)
-    <p>{{ $user->name }} - {{ $user->email }}</p>
-@endforeach
-
+<ul>
+    @if (count($users) > 2)
+        @foreach ($users as $user)
+            <li>{{ $user->name }}</li>
+        @endforeach
+    @else
+        <p>No users found.</p>
+    @endif
+</ul>
